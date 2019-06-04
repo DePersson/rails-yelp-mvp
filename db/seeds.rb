3a6baa07d3953f731f7d4cfee790c70610046bc4
Dennis Persson <depersson_81@telia.com>
2019-06-04 18:09:47 +0200

    Yelp solved

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name: 'Dishoom',
+    address: '7 Boundary St, London E2 7JE',
+    phone_number: '042 23 45 67',
+    category: 'french'
+  },
+  {
+    name: 'Basilika',
+    address: 'Vasagatan, Helsingborg',
+    category: 'chinese'
+  },
+  {
+    name: 'Le Baguettes',
+    address: 'Kungsgatan, Helsingborg',
+    category: 'french'
+  },
+  {
+    name: 'Chop sticks',
+    address: 'Söder, Helsingborg',
+    category: 'japanese'
+  },
+  {
+    name: 'Pizza East',
+    address: '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '042 23 32 11',
+    category: 'belgian'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
